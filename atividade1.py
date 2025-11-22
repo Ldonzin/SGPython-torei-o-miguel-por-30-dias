3ebc3004db7bfbd431bcaf0b6aac7d113660221c
Ldonzin <handiesel30@gmail.com>
2025-11-22 17:42:39 -0300

    Create atividade1.py

---
+++ atividade1.py
@@ -0,0 +1,11 @@
+camelCase = 'eu sei fazer print assim'
+camelCase2 = "hello world!"
+print (camelCase)
+print (camelCase2)
+GabrielWillis = "24"
+MiguelBurrao = "11"
+print("a quantidade de caracteres em camelCase é "+GabrielWillis)
+print("a quantidade de caracteres em camelCase2 é "+MiguelBurrao)
+Numbers = 24%11
+print ("loud iron melhor time do mundo")
+print (Numbers)
