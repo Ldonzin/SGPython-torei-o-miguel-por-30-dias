7fc8203357be17e2cdd70aaa5f1d4ea474f93e70
Ldonzin <handiesel30@gmail.com>
2026-02-28 18:58:30 -0300

    Add review evaluation logic in MaquinaDeAveriguarResenha

---
+++ MaquinaDeAveriguarResenha.py
@@ -0,0 +1,8 @@
+resenha = input("informe a resenha ")
+if resenha == "miguel explodiu a caveira":
+    print("resenha averiguada, miguel muito burro")
+elif resenha == "zerar o peak" or resenha == "o dia que o heitor pinou a faca":
+    print("resenha averiguada, temos uma resenha de qualidade")
+elif resenha == ("o dia que o murilo começou a namorar"):
+    print("resenha averiguada, nao temos resenha, murilo traiu a patria")
+else:   print("resenha averiguada, resenha de baixa qualidade")
